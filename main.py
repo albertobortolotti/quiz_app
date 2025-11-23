1f01d2be26384413ad6c0a8979f96f8b7d60c293
albertobortolotti <alberto.bortolotti@acus.eu>
2025-11-23 17:22:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ pages = [
         st.Page("./pages/pagina_accoglienza.py", default=True),
         st.Page("./pages/domanda_1.py"),
         st.Page("./pages/domanda_2.py"),
-        st.Page("./pages/domanda_3.py"),
-        st.Page("./pages/domanda_4.py"),
-        st.Page("./pages/domanda_5.py"),
+        #st.Page("./pages/domanda_3.py"),
+        #st.Page("./pages/domanda_4.py"),
+        #st.Page("./pages/domanda_5.py"),
         st.Page("./pages/punteggio_finale.py")
     ]
 
 pg = st.navigation(pages,position="hidden")
 
 pg.run()
+
 
