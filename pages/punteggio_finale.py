134d4d7f091fc19e36a3dc32de53a278a8259f89
albertobortolotti <alberto.bortolotti@acus.eu>
2025-11-23 17:25:43 +0100

    Add files via upload

---
+++ pages/punteggio_finale.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+message1_placeholder = st.empty()
+message2_placeholder = st.empty()
+
+
+if st.session_state['total_score'] == 50:
+    st.balloons()
+
+    message1 = '''WOW! Sei un vero esperto di Acus! 🔥<br>
+Hai superato il quiz alla grande: meriti il tuo super premio!<br>
+Vieni al nostro stand a ritirare l’ombrello (speriamo non piova… ma nel dubbio meglio averlo 😎).'''
+
+    message2 = '''Restiamo in contatto:<br>
+🔗 LinkedIn: <a href="https://www.linkedin.com/company/acus-s-p-a">www.linkedin.com/company/acus-s-p-a<a>'''
+
+
+    message1_placeholder.write(f'<p style="font-size: 20px;">{message1}</p>', unsafe_allow_html=True)
+    message2_placeholder.write(f'<p style="font-size: 20px;">{message2}</p>', unsafe_allow_html=True)
+
+else:
+    message1 = '''Oooh… per un soffio! 😅<br>
+Hai sbagliato almeno una risposta, ma niente drammi: abbiamo per te un dolce premio di consolazione!<br>
+Passa al nostro stand per ritirarlo.'''
+
+    message2 = '''Seguici per rifarti la prossima volta:<br>
+🔗 LinkedIn: <a href="https://www.linkedin.com/company/acus-s-p-a">www.linkedin.com/company/acus-s-p-a<a>'''
+
+
+
+    message1_placeholder.write(f'<p style="font-size: 20px;">{message1}</p>', unsafe_allow_html=True)
+    message2_placeholder.write(f'<p style="font-size: 20px;">{message2}</p>', unsafe_allow_html=True)
