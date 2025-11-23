ec7af721d4b993ca9ce082ec1afc6d147dd18aa0
albertobortolotti <alberto.bortolotti@acus.eu>
2025-11-23 18:02:19 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+pages = [
+        st.Page("./pages/pagina_accoglienza.py", default=True),
+        st.Page("./pages/domanda_1.py"),
+        st.Page("./pages/domanda_2.py"),
+        st.Page("./pages/domanda_3.py"),
+        st.Page("./pages/domanda_4.py"),
+        st.Page("./pages/domanda_5.py"),
+        st.Page("./pages/punteggio_finale.py")
+    ]
+
+pg = st.navigation(pages,position="hidden")
+
+pg.run()
